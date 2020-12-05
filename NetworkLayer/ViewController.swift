e40279e841db195381ea067e76818da5a73b5b33
Celal Tok <celal.tok@portalrup.com>
2020-12-05 13:46:29 +0300

    Initial Commit

---
+++ NetworkLayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkLayer
+//
+//  Created by Celal Tok on 5.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
